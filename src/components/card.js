73803c54608a1721f1e1ca1713171508b1071d93
Magda <>
2022-10-08 15:39:40 -0400

    bug fix

--- src/components/card.js
+++ src/components/card.js
@@ -13,20 +13,20 @@ const Card = () => {
 		});
 	};
 	return (
-		<main className=" w-[343px] h-[315px] md:w-[540px] md:h-[332px] bg-card-bg flex flex-col justify-center items-center p-[40px] md:p-[48px] md:pb-[72px] pb-[64px] relative">
-			<h1 className="absolute font-extrabold  text-card-title text-[11px] md:text-sm mb-[24px] top-[40px] md:top-[48px] font-Manrope md:tracking-[4.09px] tracking-[3.46px]">
+		<main className=" w-[343px]  md:w-[540px]  rounded-xl md:rounded-2xl bg-card-bg flex flex-col justify-center items-center pl-[24px] pr-[24px] md:pr-[48px] md:pl-[48px] pt-[40px] md:pt[48px]">
+			<h1 className="font-extrabold  text-card-title text-[11px] md:text-sm mb-[24px] font-Manrope md:tracking-[4.09px] tracking-[3.46px]">
 				ADVICE #{quoteid}
 			</h1>
-			<p className="text-card-quote font-extrabold text-2xl md:text-[28px] text-center font-Manrope md:tracking-[-0.3px] tracking-[-0.26px]">
+			<p className="text-card-quote font-extrabold text-2xl mb-[24px] md:mb-[40px] md:text-[28px] text-center font-Manrope md:tracking-[-0.3px] tracking-[-0.26px]">
 				"{quote}"
 			</p>
-			<section className="absolute flex flex-row items-center top-[235px] md:top-[244px]">
+			<section className=" flex flex-row items-center mb-[39px]">
 				<div className="w-[122px] md:w-[196px] h-[1px] bg-line mr-[16px]" />
 				<img src="img/Shape1.svg" alt="shape1 w-[20px] h-[16px]" />
 				<div className="w-[122px] md:w-[196px] h-[1px] bg-line ml-[16px]" />
 			</section>
 			<section
-				className="absolute top-[283px] left-[139px] md:top-[300px] md:left-[238px] rounded-full w-[64px] h-[64px] bg-card-title flex justify-center items-center hover:shadow-button-hover cursor-pointer"
+				className="mb-[-32px] rounded-full w-[64px] h-[64px] bg-card-title flex justify-center items-center hover:shadow-button-hover cursor-pointer"
 				onClick={clickHandler}
 			>
 				<img src="img/Shape.svg" alt="shape" />
